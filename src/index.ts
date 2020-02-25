e4b668292359d2c9d58c40d46f2db60d59055866
Volker Christian <volker.christian@fh-hagenberg.at>
2020-02-25 21:12:05 +0100

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata';
 import express, { Router, Request, Response, RequestHandler } from 'express';
 import { createConnection, getRepository } from 'typeorm';
-import { router, NextcloudUser } from '../nextcloud-oauth2-client/src';
+import { router, NextcloudUser, NextcloudToken } from '../nextcloud-oauth2-client/src';
 import { skill } from './lambda';
 import { ExpressAdapter } from 'ask-sdk-express-adapter';
 import { AmzNCForainKeys } from './entity/AmzNCForainKeys';
-import { NextcloudToken } from '../nextcloud-oauth2-client/src/entity/NextcloudToken';
+//import { NextcloudToken } from '../nextcloud-oauth2-client/src/entity/NextcloudToken';
 import { AmazonUser, AmazonApiEndpoint } from '../alexa-skill-user-manager/src';
 import {
     getRequestType,
@@ -35,61 +35,64 @@ import {
 
 
 const paeSend = async (amzNCForainKeys: AmzNCForainKeys) => {
-
     let expiryTime = new Date();
     expiryTime.setHours(expiryTime.getHours() + 23);
-    
-    const proactiveEventRequest: services.proactiveEvents.CreateProactiveEventRequest = {
-        event: {
-            name: 'AMAZON.TrashCollectionAlert.Activated',
-            payload: {
-                alert: {
-                    garbageTypes: ['LANDFILL', 'RECYCLABLE_PLASTICS', 'WASTE_PAPER'],
-                    collectionDayOfWeek: 'TUESDAY'
-                }
-            }
-        },
-        timestamp: new Date().toISOString(),
-        referenceId: 'wastecalendar-event-' + Date.now(),
-        expiryTime: expiryTime.toISOString(),
-        localizedAttributes: [],
-        relevantAudience: {
-            type: 'Unicast',
-            payload: {
-                user: amzNCForainKeys.amazonUser.userId
+
+    const event: services.proactiveEvents.Event = {
+        name: 'AMAZON.TrashCollectionAlert.Activated',
+        payload: {
+            alert: {
+                garbageTypes: ['LANDFILL', 'RECYCLABLE_PLASTICS', 'WASTE_PAPER'],
+                collectionDayOfWeek: 'TUESDAY'
             }
         }
     }
 
-    let result;
+    const relevantAudience: services.proactiveEvents.RelevantAudience = {
+        type: 'Unicast',
+        payload: {
+            user: amzNCForainKeys.amazonUser.userId
+        }
+    }
 
+    const proactiveEventRequest: services.proactiveEvents.CreateProactiveEventRequest = {
+        event: event,
+        timestamp: new Date().toISOString(),
+        referenceId: 'wastecalendar-event-' + Date.now(),
+        expiryTime: expiryTime.toISOString(),
+        localizedAttributes: [],
+        relevantAudience: relevantAudience
+    }
+
+    let result: services.ApiResponse;
+
+    //console.log(JSON.stringify(proactiveEventRequest, null, 4));
+
+    const apiConfiguration: services.ApiConfiguration = {
+        apiClient: new DefaultApiClient(),
+        apiEndpoint: amzNCForainKeys.amazonUser.apiEndpoint,
+        authorizationValue: 'Bearer ' + amzNCForainKeys.amazonUser.amazonApiEndpoint.apiAccessToken
+    };
+
+    const authenticationConfiguration: services.AuthenticationConfiguration = {
+        clientId: 'amzn1.application-oa2-client.c1494a447d77405883037efdc06baad6',
+        clientSecret: '07c7affba53c9d2632186cff30c678d5ed243efc6140436c533f2eac32e8dd11'
+    };
+
+    var pesc = new services.proactiveEvents.ProactiveEventsServiceClient(apiConfiguration, authenticationConfiguration);
     try {
-        console.log(JSON.stringify(proactiveEventRequest, null, 4));
-
-        const apiConfiguration: services.ApiConfiguration = {
-            apiClient: new DefaultApiClient(),
-            apiEndpoint: amzNCForainKeys.amazonUser.apiEndpoint,
-            authorizationValue: 'Bearer ' + amzNCForainKeys.amazonUser.amazonApiEndpoint.apiAccessToken
-        };
-
-        const authenticationConfiguration: services.AuthenticationConfiguration = {
-            clientId: 'amzn1.application-oa2-client.c1494a447d77405883037efdc06baad6',
-            clientSecret: '07c7affba53c9d2632186cff30c678d5ed243efc6140436c533f2eac32e8dd11'
-        };
-
-        var pesc = new services.proactiveEvents.ProactiveEventsServiceClient(apiConfiguration, authenticationConfiguration);
         result = await pesc.callCreateProactiveEvent(proactiveEventRequest, 'DEVELOPMENT');
     } catch (error) {
         console.log("Error: " + error);
     }
-    console.log("-------- " + JSON.stringify(result, null, 4));
+    console.log("Status Code: " + JSON.stringify(result.statusCode, null, 4));
 }
 
 
 const paeProcess: RequestHandler = (req: Request, res: Response) => {
     const skillId = "amzn1.ask.skill.5119403b-f6c6-45f8-bd7e-87787e6f5da2";
 
-    let str: string;
+    let str: string = "";
 
     getRepository<AmzNCForainKeys>('AmzNCForainKeys')
         .createQueryBuilder('link')
@@ -121,14 +124,12 @@ const paeProcess: RequestHandler = (req: Request, res: Response) => {
                 str += "==============================\n"
                 str += JSON.stringify(value, null, 4) + "\n";
                 str += "==============================\n";
-
-                console.log("=============================");
-//                console.log(JSON.stringify(value, null, 4));
-                console.log("=============================");
-
                 paeSend(value);
             });
             res.send(str);
+        })
+        .catch(reason => {
+            res.status(500).send(reason);
         });
 }
 
